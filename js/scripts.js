67424567b43c2174f4dd83566647442f2df1e2e8
user-name <christine.clark@uleth.ca>
2024-11-20 16:45:35 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+console.log("bonjour")
+
+console.log("oki")
+
+$("#num-19").append("<h2>World Toilet Day</h2>")
+$("#num-19").css("background-image","url(imgs/wtd.webp)")
+$("#num-19").css("background-size","cover")
+
+$("#num-20").addClass("wcd-bkg")
