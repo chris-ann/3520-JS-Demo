777f5644b42140426a8124855143707f338e49d9
user-name <christine.clark@uleth.ca>
2024-11-22 16:36:09 -0700

    finished example

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,10 @@ console.log("oki")
 $("#num-19").on("click", function () {
     console.log("clicked on box 19")
     $(this).html("November 19<h2>World Toilet Day</h2>")
-    $(this).css("background-image", "url(imgs/wtd.webp)")
-    $(this).css("background-size", "cover")
+    $(this).css("background-image", "url(imgs/wtd.webp)");
+    $(this).css("background-size", "cover");
+    $(this).css("color", "white");
+    $(this).css("text-shadow", "1px 1px 2px rgba(0,0,0,0.5)");
 })
 
 $("#num-20").on("click", function () {
@@ -15,17 +17,37 @@ $("#num-20").on("click", function () {
     $(this).unbind("click")
 })
 
+$("#num-25").on("click", function () {
+    $("#num-25").append("<h2>International Day for the Elimination of Violence against Women</h2>")
+    $("#num-25").append("<a href='https://www.un.org/en/observances/ending-violence-against-women-day'>Learn More</a>")
+    $("#num-25").css("background", "url('imgs/no-excuse.png')");
+    $(this).unbind("click");
+})
 
+// Practice with Keypress Events
+$(document).on("keypress", function (e) {
+    console.log(e.key)
+    if (e.key == "t") {
+        $("#num-21").html("November 21<h2>World Television Day</h2>")
+        $("#num-21").addClass("tv");
+    } else if (e.key == "s") {
+        $("#num-26").html("<img src='imgs/bus.png' class='drive'>November 26<h2>World Sustainable Transportation Day</h2>")
+        $("#num-26").prepend("")
+        $("#num-26").css("background", "#B7E5E5");
+    }
+})
+
+// named function example
 $(".box").on("click", addBorder);
 
 function addBorder() {
     $(this).css("border", "2px solid black");
 }
 
+// Hide non-days button
 var days = true;
-
 $("#hide-days").on("click", function () {
-    $("#num-22, #num-23, #num-24").fadeToggle(3000);
+    $("#num-22, #num-23, #num-24").fadeToggle(1000);
 
     if (days == true) {
         $(this).text("Show Non-Days")
@@ -35,13 +57,4 @@ $("#hide-days").on("click", function () {
         days = true;
     }
 
-})
-
-$(document).on("keypress", function (e) {
-    console.log(e.key)
-    if (e.key == "t") {
-        $("#num-21").html("November 21<h2>World Television Day</h2>")
-    } else if (e.key == "s") {
-        $("#num-26").html("November 26<h2>World Sustainable Transportation Day</h2>")
-    }
 })
